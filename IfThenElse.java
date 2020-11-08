a0f175777df8809799bc87eb13ed08ed10bc5bf8
KholisEnka22 <enka.run2206@gmail.com>
2020-11-08 19:34:30 +0700

    Tugas Ketiga

---
+++ IfThenElse.java
@@ -0,0 +1,12 @@
+package percabangan;
+
+public class IfThenElse {
+    public static void main(String[]args){
+        boolean isOn = false;
+        if (isOn) {
+            System.out.println("Menyalakan Lampu!");
+        } else {
+            System.out.println("Kondisi Tidak Terpenuhi...");
+        }
+    }
+}
